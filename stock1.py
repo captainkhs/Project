b770107d3104a3eb7adb2ee6d7658073a69ce694
captainkhs <62538707+captainkhs@users.noreply.github.com>
2020-06-21 18:51:55 +0900

    Update stock1.py

--- stock1.py
+++ stock1.py
@@ -14,21 +14,18 @@ import requests
 import FinanceDataReader as fdr
 from datetime import date, timedelta
 
-app = Flask(__name__)
-
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 d = date.today() - timedelta(15)
 Date = d.isoformat()
-
-
-name = ''
-symbol = 'MMM'
-
-symbol = name or symbol
+ # 1. 클라이언트가 전달한 'name', 'code' 를 name, code 변수에 넣습니다.
+name = 'Jack Henry & Associates, Inc.'
+symbol = db.STOCK.find_one({'Name': name}, {'_id':False})
+symbol = symbol['Symbol']
 
 df = fdr.DataReader(symbol, Date)
-df = df['Close'].tolist()
-print(df)
+# df = df
+# df = df.tolist()
+print(df.index.strftime('%Y-%m-%d').values)
 
