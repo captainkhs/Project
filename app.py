93a9527f8cfb09beb112576440b65521b3be8918
captainkhs <62538707+captainkhs@users.noreply.github.com>
2020-06-21 13:52:43 +0900

    mongo url edit

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ import numpy as np
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost',27017)
 db = client.dbsparta
 
 @app.route('/')
