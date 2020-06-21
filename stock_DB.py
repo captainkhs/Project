e3a7fcda5e865ca06238df97e2c3b27fcd6be8d5
captainkhs <62538707+captainkhs@users.noreply.github.com>
2020-06-21 13:28:51 +0900

    Create stock_DB.py

---
+++ stock_DB.py
@@ -0,0 +1,30 @@
+import FinanceDataReader as fdr
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+db.user.drop()
+
+# 한국
+# 심볼	거래소
+# KRX	KRX 종목 전체
+# KOSPI	KOSPI 종목
+# KOSDAQ	KOSDAQ 종목
+# KONEX	KONEX 종목
+# 미국
+# 심볼	거래소
+# NASDAQ	나스닥 종목
+# NYSE	뉴욕 증권거래소 종목
+# AMEX	AMEX 종목
+# SP500	S&P 500 종목
+df = fdr.StockListing('S&P500')
+symbol = df['Symbol']
+name = df['Name']
+
+for i in range(len(df['Symbol'])):
+    doc = {
+    'Symbol' :symbol[i],
+    'Name' :name[i] 
+    }   
+    db.STOCK.insert_one(doc)
+    print(doc)
