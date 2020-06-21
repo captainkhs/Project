2d237b7061f5d2839ab766e8ead255d925d03599
captainkhs <62538707+captainkhs@users.noreply.github.com>
2020-06-21 14:49:46 +0900

    Update stock_DB.py

--- stock_DB.py
+++ stock_DB.py
@@ -17,7 +17,7 @@ db = client.dbsparta
 # NYSE	뉴욕 증권거래소 종목
 # AMEX	AMEX 종목
 # SP500	S&P 500 종목
-df = fdr.StockListing('NYSE')
+df = fdr.StockListing('AMEX')
 symbol = df['Symbol']
 name = df['Name']
 
