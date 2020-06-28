d602985b5710330e1323edfba3cb3482b7d64808
captainkhs <62538707+captainkhs@users.noreply.github.com>
2020-06-28 13:25:14 +0900

    Update news_db.py

--- news_db.py
+++ news_db.py
@@ -1,114 +1,88 @@
-import os
-import sys
-import urllib.request
-from urllib.parse import *
 import requests
 from bs4 import BeautifulSoup
-import json
-import re
+import bs4.element
+import datetime
 
-# 오픈 API 아이디, 비번 (최초 설정 후 건드리지 말 것)
-client_id = "xxxxxxxxxxxx"
-client_secret = "xxxxxxxxxxxx"
+from pymongo import MongoClient
+client = MongoClient('localhost',27017)
+db = client.dbsparta
 
-# keyword_1 : 첫번째 키워드
-# keyword_2 : 두번째 키워드
-# keyword_3 : 세번째 키워드
-# 첫번째 키워드 + 두번째 키워드로 검색이 진행됩니다.
-# 예를 들어, 첫번째 키워드가 ['개', '고양이', '토끼'] 이고
-# 두번째 키워드가 ['1', '2'] 이면,
-# 개 1, 개 2, 고양이 1, 고양이 2, 토끼 1, 토끼 2 처럼
-# 키워드 1 개수 * 키워드 2 개수 만큼 검색 쿼리가 생성됩니다.
-# 세번째 키워드는, 추가적으로 단일 키워드를 검색하고 싶은 경우 활용합니다.
+# '경제', '사회', '생활/문화', '세계', 'IT/과학'
+# 각 분야의 상위 3개 뉴스들을 가져옵니다. (5분야 * 3개 = 15개)
+def get_naver_news_top3():
+    db.NEWS.drop()
+    
+    # 뉴스를 가져올 링크 주소 지정
+    base_url = "https://news.naver.com"
+    today = datetime.datetime.today().strftime('%Y%m%d')
+    news_params = "/main/ranking/popularDay.nhn?rankingType=popular_day&date=" + today + "&sectionId="
+        
+    
+    # 뉴스 결과를 담아낼 dictionary
+    news_dic = dict()
+    
+    # sections : '경제', '사회', '생활/문화', '세계', 'IT/과학'
+    # section_ids :  URL에 사용될 뉴스  각 부문 ID
+    # sections = ["eco","soc","lif","wor","its"]
+    # section_ids = ["101","102","103","104","105"]
+    sections = ["eco"]
+    section_ids = ["101"]
+    for sec, sid in zip(sections, section_ids):
+        # 해당 분야 상위 뉴스 목록 주소
+        news_link = base_url + news_params + sid
+        
+        
+        # 해당 분야 상위 뉴스 HTML 가져오기
+        res = requests.get(news_link)
+        soup = BeautifulSoup(res.text,'lxml')
+        
+        # 해당 분야 상위 뉴스 3개 가져오기
+        lis3 = soup.find_all('li', class_='ranking_item', limit=10)
+        
+        
+        # 가져온 뉴스 데이터 정제하기
+        news_list3 = []
+        default_img = "https://search.naver.com/search.naver?where=image&sm=tab_jum&query=naver#"
+        for li in lis3:
+            news_link = base_url + li.a.attrs.get('href')
+            
+            res = requests.get(news_link)
+            soup = BeautifulSoup(res.text,'lxml')
+            body = soup.find('div', class_="_article_body_contents")
+            
+            # 뉴스 본문 가져오기
+            news_body = ''
+            for content in body:
+                if type(content) is bs4.element.NavigableString and len(content) > 50:
+                    # content.strip() : whitepace 제거 (참고 : https://www.tutorialspoint.com/python3/string_strip.htm)
+                    news_body += content.strip() + ' '
+                    # news_body += content.strip()
+                    # 뉴스 요약할 때, '.' 마침표 뒤에 한칸 띄워주지 않으면 문장을 구분 못하는 경우가 있음
+                        
+            # title : 뉴스 제목
+            # views : 조회수
+            # link : 뉴스 URL
+            # news_body : 뉴스 내용
+            # image_url : 이미지 URL
+            news_info = {
+                "title" : li.a.attrs.get('title'),
+                "views" : li.find('div', class_="ranking_view").text,
+                "link" : news_link,
+                "news_body" : news_body[0:50],
+                "image_url" : li.img.attrs.get('src') if li.img else default_img
+            }
+            
+            news_list3.append(news_info)
+            
+            db.NEWS.insert_one(news_info)
+        news_dic[sec] = news_list3
+        
 
-keyword_1 = ['강아지', '고양이']
-keyword_2 = ['토끼', '공원']
+    return news_dic
 
+news_dic = get_naver_news_top3()
 
-display = 30 # 각 키워드 당 검색해서 저장할 기사 수
-
-
-
-#
-#
-# 여기서부터는 코드입니다 (신경 안쓰셔도 됩니다!)
-#
-#
-
-
-# Input(str) : 뉴스에 검색할 단어 넣기
-def news_search(min_name):
-    encText = urllib.parse.quote(min_name)
-    url = "https://openapi.naver.com/v1/search/news.json?query=" + encText + \
-        "&display=" + str(display) + "&sort=sim"
-         # json 결과
-    # url = "https://openapi.naver.com/v1/search/news.xml?query=" + encText # xml 결과
-    request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id",client_id)
-    request.add_header("X-Naver-Client-Secret",client_secret)
-
-
-    response = urllib.request.urlopen(request)
-    rescode = response.getcode()
-    if(rescode==200):
-        #response_body_str = response.read().decode('utf-8')
-        response_body_str = response.read().decode('utf-8')
-        json_acceptable_string = response_body_str.replace("'", "\"")
-        response_body = json.loads(response_body_str)
-        title_link = {}
-        for i in range(0, len(response_body['items'])):
-            title_link[response_body['items'][i]['title']] = \
-                response_body['items'][i]['link']
-        return title_link
-
-    else:
-        print("Error Code:" + rescode)
-
-
-# extract urls from correspoding response_body
-def get_url(resbody):
-    title_link = {}
-    #print(str(len(resbody['items'])) + ' length of resbody of items')
-    for i in range(0, len(resbody['items'])):
-        #print(resbody['items'][i]['title'])
-        title_link[resbody['items'][i]['title']] = resbody['items'][i]['link']
-    return title_link
-
-
-# using url list, crawl html and store them as .html
-def url_to_html(title_links, cur_keyword1):
-    # using url, store them into .html
-    # 1. Make a single html file and add links to it
-    filename = re.sub("[\/:*?\"<>|]", "", cur_keyword1)
-    f = open(filename + '.html', 'w', encoding='UTF-8')
-    for k in title_links.keys():
-        '''
-        # 2. Make seperate folder for each keyword combiantion
-        filename = re.sub("[\/:*?\"<>|]", "", k)
-        path = os.path.abspath(".\\" + cur_keyword1)
-        f = open(os.path.join(path, filename) +'.html', 'w', encoding='UTF-8')
-        '''
-        f.write("<A href=" + \
-            title_links[k] + ">" + \
-                k + "</A>" + "<br></br>")
-
-# 키워드 1 + 키워드 2 조합으로 검색
-def keyword_combined(keyword_1, keyword_2):
-    for i in range(len(keyword_1)):
-        '''
-        # Make folders for each articles
-        # Uncomment it when you use "2" in url_to_html function
-        if not os.path.exists(keyword_1[i]):
-            os.makedirs(keyword_1[i])
-        '''
-        title_links = {}
-        if len(keyword_2) == 0:
-            title_links = {**title_links, **(news_search(keyword_1[i]))}
-        else:
-            for j in range(len(keyword_2)):
-                title_links = {**title_links, **(news_search(keyword_1[i] + " " + keyword_2[j]))}
-        url_to_html(title_links, keyword_1[i])
+news_dic['eco'][0].keys()
 
 
-if __name__ == '__main__':
-    keyword_combined(keyword_1, keyword_2)
+    
