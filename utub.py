896ceb7102d0909c792625e45cf1e2cca1bd47c5
captainkhs <62538707+captainkhs@users.noreply.github.com>
2020-06-28 18:14:57 +0900

    Update utub.py

--- utub.py
+++ utub.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from flask import Flask, render_template, jsonify, request
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost',27017)
 db = client.dbsparta
 
 from googleapiclient.discovery import build
@@ -10,43 +10,39 @@ from oauth2client.tools import argparser
 import datetime, time
 import schedule
 
-# def job():
-#     DEVELOPER_KEY = "AIzaSyAmmHbqw6Dz1JH_qNs5IzTLy4gt7O7HAEo"
-#     YOUTUBE_API_SERVICE_NAME = "youtube"
-#     YOUTUBE_API_VERSION = "v3"  
-#     youtube = build(YOUTUBE_API_SERVICE_NAME,YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
-#     search_response = youtube.search().list(q = "미국주식", order = "date", part = "snippet", maxResults = 10).execute()
-#     videos = []
-#     channels = []
-#     playlists = []
-#     for search_result in search_response.get("items", []):
-#         if search_result["id"]["kind"] == "youtube#video":
-#             videos.append("%s $(%s)" % (search_result["snippet"]["title"],search_result["id"]["videoId"]))
-#             doc = {
-#                             'VIDEOID' : search_result["id"]["videoId"],
-#                             'TITLE' : search_result["snippet"]["title"],
-#                             'URL' : search_result["snippet"]["thumbnails"]["medium"]["url"]
-#                          }
+def job():
+    DEVELOPER_KEY = "AIzaSyAmmHbqw6Dz1JH_qNs5IzTLy4gt7O7HAEo"
+    YOUTUBE_API_SERVICE_NAME = "youtube"
+    YOUTUBE_API_VERSION = "v3"  
+    youtube = build(YOUTUBE_API_SERVICE_NAME,YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+    search_response = youtube.search().list(q = "미국주식", order = "date", part = "snippet", maxResults = 10).execute()
+    videos = []
+    channels = []
+    playlists = []
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos.append("%s $(%s)" % (search_result["snippet"]["title"],search_result["id"]["videoId"]))
+            doc = {
+                            'VIDEOID' : search_result["id"]["videoId"],
+                            'TITLE' : search_result["snippet"]["title"],
+                            'URL' : search_result["snippet"]["thumbnails"]["medium"]["url"]
+                         }
             
 
-#         elif search_result["id"]["kind"] == "youtube#channel":
-#             channels.append("%s (%s)" %(search_result["snippet"]["title"], search_result["id"]["channelID"]))
+        elif search_result["id"]["kind"] == "youtube#channel":
+            channels.append("%s (%s)" %(search_result["snippet"]["title"], search_result["id"]["channelID"]))
         
-#         elif search_result["id"]["kind"] == "youtube#playlist":
-#             playlists.append("%s (%s)" %(search_result["snippet"]["title"], search_result["id"]["playlistID"]))
-#         db.videoList.insert_one(doc)      
-#         print(doc)
+        elif search_result["id"]["kind"] == "youtube#playlist":
+            playlists.append("%s (%s)" %(search_result["snippet"]["title"], search_result["id"]["playlistID"]))
+        db.videoList.insert_one(doc)      
+        print(doc)
 
-# def run():
-#     # schedule.every().day.at('09:00').do(job)
-#     schedule.every(10).seconds.do(job)
-#     while True:
-#         schedule.run_pending()
+def run():
+    # schedule.every().day.at('09:00').do(job)
+    schedule.every(10).seconds.do(job)
+    while True:
+        schedule.run_pending()
 
-# if __name__ == "__main__":
-#     run()
+if __name__ == "__main__":
+    run()
 
-
-utube = list(db.videoList.find({},{'_id':False}))
-for i in utube:  
-    print(i)
